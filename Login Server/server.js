929e15f1221903af00803c3481671a9bedeed2ad
DM5555 <dm4x5@outlook.com>
2019-09-15 23:24:43 +0100

    upload code that doesnt work

--- Login Server/server.js
+++ Login Server/server.js
@@ -2,13 +2,40 @@
 
 const https = require('https');
 const fs = require('fs');
+const url = require('url');
+const path = require('path');
 
-const loginPage = fs.readFileSync("./login.html");
+
+/* =================
+DOESNT WORK NEED TO FIX
+==================*/
+
+function evaluatePath(input){ //Turn a url into a useable path
+  if (input == ""){
+    return "/";
+  } else {
+    var pathname =  url.parse(input).pathname;
+    if (pathname == ""){
+      return "/";
+    } else {
+      return path.posix.normalize(pathname); //Removes any relative paths.
+    }
+  }
+}
+
+function error404(res){
+  res.writeHead(404);
+  res.end("404: Page not found!");
+}
+
 
 https.createServer({
   cert: fs.readFileSync("./.secret/MyCertificate.crt"),
   key: fs.readFileSync("./.secret/MyKey.key")
 },(req,res) => {
+  var ep = evaluatePath(req.url);
+
   res.writeHead(200);
-  res.end(loginPage);
+  res.end(ep);
+
 }).listen(443);
